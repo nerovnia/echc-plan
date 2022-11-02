a346a731a7a6dccf5b7210ca0e5c673015de5d24
Volodymyr Nerovnia <nerv@i.ua>
2022-11-02 12:11:13 +0200

    Import siquelize in to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,30 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { ApiController } from './api.controller';
+import { ApiService } from './api.service';
+
+//import { databaseProviders } from './database.providers';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+
+//import { AppDatabase } from './ database.providers';
+//import { RouterModule } from '@nestjs/core';
+//import { ApiModule } from './api.module';
+
+
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ApiController],
+  providers: [AppService, ApiService],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: `.${process.env.NODE_ENV}.env` }),
+    SequelizeModule.forRoot({
+      dialect: 'sqlite',
+      storage: process.env.STORAGE,
+      autoLoadModels: true,
+      logging: (...msg) => console.log(msg),
+    }),
+  ],
 })
 export class AppModule {}
